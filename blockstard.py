3bcb8a465eada216439293494d72d43fc99c3ee2
Andrei Stoian <andrei.stoian@zama.ai>
2022-12-29 09:06:57 +0100

    add daily graphs

--- blockstard.py
+++ blockstard.py
@@ -67,9 +67,7 @@ def rtl_433_probe(args):
             battery_ok = data["battery_ok"]
 
             if sensor_id in whitelist_ids:
-                out_dir = os.path.join(
-                    args.db_dir, f"{date_rec.year}_{date_rec.month}_{date_rec.day}"
-                )
+                out_dir = os.path.join(args.db_dir, date_rec.strftime("%Y_%m_%d"))
                 out_file = os.path.join(out_dir, "indoors.csv")
                 out_line = (
                     ",".join(map(lambda v: str(v), [date_rec, sensor_id, temperature, battery_ok]))
@@ -126,7 +124,14 @@ def render_graph(day_start_time, day_end_time, sensor_name, times, temps, graph_
     plt.savefig(graph_file)
 
 
-def make_graphs(date_dir):
+def is_file_older(file, hours=1):
+    ti_c = os.path.getctime(file)
+    c_ti = time.ctime(ti_c)
+    graph_creation_time = time.strptime(c_ti)
+    return datetime.today() - graph_creation_time > timedelta(hours=hours)
+
+
+def make_graphs(date_dir, is_today):
     log_data = pandas.read_csv(os.path.join(date_dir, INDOORS_LOG))
     unique_ids = log_data["sensor_id"].unique()
 
@@ -138,9 +143,11 @@ def make_graphs(date_dir):
     all_temps = []
     all_titles = []
 
+    use_dir = date_dir if not is_today else os.path.dirname(date_dir)
+
     for sensor_id in unique_ids:
         graph_file = os.path.join(
-            date_dir, INDOORS_GRAPH_PREFIX + str(sensor_id) + INDOORS_GRAPH_EXT
+            use_dir, INDOORS_GRAPH_PREFIX + str(sensor_id) + INDOORS_GRAPH_EXT
         )
 
         sensor_values = log_data[log_data["sensor_id"] == sensor_id]
@@ -149,18 +156,32 @@ def make_graphs(date_dir):
 
         graph_title = str(sensor_id) + ": " + whitelist_ids[sensor_id]
         # Do not regenerate the graph if it exists
+
+        render_this_graph = False
         if not os.path.exists(graph_file):
+            # If today's data and the graph does not exist, make it 
+            # if it's yesterday's data and the graph does not exist make it
+            render_this_graph = True
+        elif is_today and is_file_older(graph_file):
+            # Current day's graph, if it's more than 1 hour old, make it, save to root DB dir
+            render_this_graph = True
+
+        if render_this_graph:
             render_graph(day_start_time, day_end_time, graph_title, times, temps, graph_file)
 
         all_times.append(times)
         all_temps.append(temps)
         all_titles.append(graph_title)
 
-    graph_file = os.path.join(date_dir, INDOORS_GRAPH_PREFIX + "all" + INDOORS_GRAPH_EXT)
+    graph_file = os.path.join(use_dir, INDOORS_GRAPH_PREFIX + "all" + INDOORS_GRAPH_EXT)
+    render_this_graph = False
     if not os.path.exists(graph_file):
-        render_graph(
-            day_start_time, day_end_time, "ALL", all_times, all_temps, graph_file, all_titles
-        )
+        render_this_graph = True
+    elif is_today and is_file_older(graph_file):
+        # Current day's graph, save to root DB dir
+        render_this_graph = True
+
+    render_graph(day_start_time, day_end_time, "ALL", all_times, all_temps, graph_file, all_titles)
 
 
 def gen_graphs_thread(args):
@@ -173,9 +194,8 @@ def gen_graphs_thread(args):
             try:
                 date_dir = datetime.strptime(os.path.basename(file), "%Y_%m_%d")
                 # If it's a log directory with a date its name, check it's earlier than today
-                if date_dir >= midnight:
-                    continue
-                make_graphs(file)
+                is_today = date_dir >= midnight
+                make_graphs(file, is_today)
             except Exception as err:
                 # Ignore exceptions (bad date format, bad data, etc)
                 traceback.print_exc()
